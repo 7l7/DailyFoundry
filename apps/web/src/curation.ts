4cdbd04d0c936ce1db277799c7eb880916f180bd
7l7 <37323278+7l7@users.noreply.github.com>
2026-09-16 11:11:36 +0800

    refactor: expose curated question status as typed sets

---
+++ apps/web/src/curation.ts
@@ -0,0 +1,27 @@
+export const specialistIds = new Set<string>([
+  "extra-1969-arpanet",
+  "extra-1971-email",
+  "extra-1973-ethernet",
+  "web-1983",
+  "web-1984",
+  "web-1986",
+  "web-1988",
+  "extra-1990-first-web-server",
+  "extra-1991-linux",
+  "extra-1993-web-public-domain",
+  "extra-1994-banner-ad",
+  "extra-1999-salesforce",
+  "extra-2002-tor",
+  "extra-2006-aws-s3"
+]);
+
+export const retiredIds = new Set<string>([
+  "extra-1984-macintosh",
+  "extra-1989-gameboy",
+  "extra-2003-skype",
+  "extra-2016-messenger-bots",
+  "fun-2010-double-rainbow",
+  "fun-2014-twitch-plays-pokemon",
+  "fun-2015-periscope",
+  "fun-2017-hq-trivia"
+]);
